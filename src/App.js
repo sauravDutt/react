83b1515086199220b704457ca08638f94a31fdc5
sauravDutt <sauravd069@gmail.com>
2023-08-15 15:19:57 +0530

    just added some more stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from './components/Header';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+
+    return(
+        <div className="header-outter">
+
+        </div>
+    );
+}
+
+export default Header;
