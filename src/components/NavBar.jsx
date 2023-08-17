51ef6d6d2dd8e4f1a500b5bdb4e5f488e84fca7b
sauravDutt <sauravd069@gmail.com>
2023-08-18 01:43:59 +0530

    Just added some more stuff

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,13 @@ const NavBar = () => {
 
     return(
         <div className="navbar-outter">
-            <div className="app-title">
-                <h1 className="brand-name">Gtown Journal</h1>
-                <h3 className="lower">
-                    SauravDutt<span className="secondFont">com</span>
-                </h3>
+            <div>
+                <div className="app-title">
+                    <h1 className="brand-name">Gtown Journal</h1>
+                    <h3 className="lower">
+                        SauravDutt<span className="secondFont">com</span>
+                    </h3>
+                </div>
             </div>
         </div>
     );
