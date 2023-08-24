1ccf151331e91b364a000526976f7bd918ac4bd8
sauravDutt <sauravd069@gmail.com>
2023-08-24 17:14:16 +0530

    Just added some more stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,12 @@ const Header = () => {
 
     return(
         <div className="header-outter">
-
+            <div className="buttons-outter">
+                <div className="button-outter"></div>
+                <div className="button-outter"></div>
+                <div className="button-outter"></div>
+                <div className="button-outter"></div>
+            </div>
         </div>
     );
 }
