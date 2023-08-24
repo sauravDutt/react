950794525089174ace759370535d5c7e0fa7254d
sauravDutt <sauravd069@gmail.com>
2023-08-25 00:43:55 +0530

    Just added some more stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
-import Header from './components/Header';
-import MainAreaOne from './components/MainAreaOne';
-import NavBar from './components/NavBar';
+// import Header from './components/Header';
+// import MainAreaOne from './components/MainAreaOne';
+// import NavBar from './components/NavBar';
 import SignIn from './components/signin';
 import './index.css';
 
