3cc5ff70eaefe31b5ac24dde2089b856837b9dac
sauravDutt <sauravd069@gmail.com>
2023-08-24 18:17:05 +0530

    Just added some more stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import PostAddRoundedIcon from '@mui/icons-material/PostAddRounded';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
 
 const Header = () => {
 
     return(
         <div className="header-outter">
             <div className="buttons-outter">
-                <div className="button-outter"></div>
-                <div className="button-outter"></div>
-                <div className="button-outter"></div>
-                <div className="button-outter"></div>
+                <div className="button-outter"><HomeRoundedIcon fontSize='large' sx={{width: '100%'}}/></div>
+                <div className="button-outter"><PostAddRoundedIcon fontSize='large' sx={{width: '100%'}}/></div>
+                <div className="button-outter"><StorefrontOutlinedIcon fontSize='large' sx={{width: '100%'}}/></div>
+                <div className="button-outter"><AccountCircleOutlinedIcon fontSize='large' sx={{width: '100%'}}/></div>
             </div>
         </div>
     );
