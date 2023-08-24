572b0ac9dc2345027e26e4fef0397c8353280497
sauravDutt <sauravd069@gmail.com>
2023-08-25 00:35:44 +0530

    Just added some more stuff

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import Header from './components/Header';
 import MainAreaOne from './components/MainAreaOne';
 import NavBar from './components/NavBar';
+import SignIn from './components/signin';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
-    <Header />
+    {/* <Header />
     <NavBar />
-    <MainAreaOne/>
+    <MainAreaOne/> */}
+    <SignIn />
     </div>
   );
 }
---
+++ src/components/signin.jsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import NavBar from './NavBar';
+import Header from './Header';
+
+
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 12,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+        <NavBar />
+        <AccountCircleOutlinedIcon fontSize='large' sx={{mt:7 ,width: '100%', fontSize: 50}}/>
+          
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, background: '#000'}}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2" sx={{color: '#000', textDecoration: 'none'}}>
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2" sx={{color: '#000', textDecoration: 'none'}}>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+      <Header />
+    </ThemeProvider>
+  );
+}
