804b0bc68b6f901531f915854010460ae5935fbd
sauravDutt <sauravd069@gmail.com>
2023-08-25 18:48:23 +0530

    Just added some more stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
+// import { useState } from 'react';
 import Header from './components/Header';
 // import LogIn from './components/LogIn';
 import MainAreaOne from './components/MainAreaOne';
 import NavBar from './components/NavBar';
 import './index.css';
 
+
+
 function App() {
+  // const [login, setLogin] = useState(0);
+
   return (
     <div className="App">
     <Header />
