e3565159bcc15017a2864abaf668a42c73c48400
sauravDutt <sauravd069@gmail.com>
2023-09-07 03:16:07 +0530

    Just added some more stuff

--- src/components/UserDashboardPage.jsx
+++ src/components/UserDashboardPage.jsx
@@ -38,7 +38,16 @@ const UserDashboardPage = ({setIsAuth, userData}) => {
                     electronic typesetting, remaining essentially unchanged. It was popularised in 
                     the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
                     and more recently with desktop publishing software like Aldus PageMaker including 
-                    versions of Lorem Ipsum.
+                    versions of Lorem Ipsum. <span>Gtown Journal</span> is simply <b>dummy text</b> of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type 
+                    specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in 
+                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including 
+                    versions of Lorem Ipsum. <span>{userData.user.displayName}</span> is simply dummy text of the printing and typesetting industry. 
+                    <b> Lorem Ipsum</b> has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type 
                 </p>
             </div>
         </div>
