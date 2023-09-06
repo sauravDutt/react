860e6c1ab8e30fbac0360c497664e496ba7275b1
sauravDutt <sauravd069@gmail.com>
2023-09-07 02:55:52 +0530

    Just added some more stuff

--- src/components/UserDashboardPage.jsx
+++ src/components/UserDashboardPage.jsx
@@ -1,6 +1,7 @@
 import {signOut} from 'firebase/auth'
 import {useNavigate} from 'react-router-dom';
 import { auth } from '../firebase-config';
+import gal from '../img/gal.png'
 
 
 const UserDashboardPage = ({setIsAuth, userData}) => {
@@ -26,7 +27,17 @@ const UserDashboardPage = ({setIsAuth, userData}) => {
                 <div className='onlineDot'></div>
             </div>
             <div className='userStats'>
-
+                <img src={gal} className='backgroundImgDashboard' alt='background img'/>
+                <p>
+                    <span>Welcome </span>is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type 
+                    specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in 
+                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including 
+                    versions of Lorem Ipsum.
+                </p>
             </div>
         </div>
     );
