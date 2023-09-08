c4018ca59d1798fe4a9deabe57d707cd8068e003
sauravDutt <sauravd069@gmail.com>
2023-09-08 14:24:43 +0530

    Just added some more stuff

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,18 +2,22 @@
 const News = ({news}) => {
 
     return(
-        <div className="newsPost">
-            {/* <h1>{news.title}</h1>
-            <h4>{news.author}</h4>
-            <h5>{news.publishedAt}</h5>
-            <p>
-                {news.description}
-            </p>
-            <p>
-                {news.content}
-            </p>
-            <a href={news.url} target="blank" >Read More</a> */}
+        <div className="news-outter">
+            <h1><sup>Daily</sup>news</h1>
+            <div className="newsPost">
+                {/* <h1>{news.title}</h1>
+                <h4>{news.author}</h4>
+                <h5>{news.publishedAt}</h5>
+                <p>
+                    {news.description}
+                </p>
+                <p>
+                    {news.content}
+                </p>
+                <a href={news.url} target="blank" >Read More</a> */}
+                </div>
         </div>
+        
     );
 }
 
