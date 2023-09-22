00ae23dffe580bea27df2a433208bef5eb0a9dfe
sauravDutt <sauravd069@gmail.com>
2023-09-22 18:38:14 +0530

    Just added some more stuff

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,9 +1,17 @@
+import SendIcon from '@mui/icons-material/Send';
 
-const News = ({news}) => {
+const News = () => {
 
     return(
         <div className="news-outter">
             <h1><sup>Chatbot </sup><span>powered by <strong>Google Bard</strong></span></h1>
+            <div className="chatbot-outter">
+
+            </div>
+            <div className="chatbot-input">
+                <input type='text'/>
+                <button><SendIcon fontSize='large'/></button>
+            </div>
         </div>
         
     );
