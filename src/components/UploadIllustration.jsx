6aa5ed36d08b9e6203363df7734ea9b22daf3cf7
sauravDutt <sauravd069@gmail.com>
2023-09-22 21:39:26 +0530

    Just added some more stuff

--- src/components/UploadIllustration.jsx
+++ src/components/UploadIllustration.jsx
@@ -12,10 +12,17 @@ const UploadIllustration = ({isAuth}) => {
         }
 
     });
-    
+
     return(
         <div className="form-outter">
-            <h1>Upload illustration form</h1>
+            <h1>Upload an <sub> Illutstration</sub></h1>
+            <div>
+                <label>Upload a file </label>
+                <input type="file"/>
+                <label>Caption</label>
+                <input type="text"/>
+                <button>upload</button>
+            </div>
         </div>
     );
 }
