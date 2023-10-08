91160a2c161b77f3a1d0dc8b6fa022f83648b951
sauravDutt <sauravd069@gmail.com>
2023-10-08 14:54:02 +0530

    added the Canvas element from react-three-fiber

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,13 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 
 const Game = () => {
     return(
-        <>
+        <Suspense fallback={null}>
+            <Canvas className="mainCanvas">
 
-        </>
+            </Canvas>
+        </Suspense>
     );
 }
 
