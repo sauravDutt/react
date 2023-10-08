040482315677c39e7eeb4a63801becd67c05d1e3
sauravDutt <sauravd069@gmail.com>
2023-10-08 23:01:48 +0530

    Just added some more stuff

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,23 +1,26 @@
-import { OrbitControls, Sphere } from "@react-three/drei";
+import { Environment, OrbitControls, Sphere, useEnvironment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useControls } from "leva";
+// import { useControls } from "leva";
 import { Suspense } from "react";
 
 const ReflectiveSphere = () => {
 
-    const tweakableProperties = useControls({
-        roughness: {value: 0.1, min: 0, max: 1},
-        metalness: {value: 1, min: 0, max: 1},
-    })
+    // const tweakableProperties = useControls({
+    //     roughness: {value: 0.1, min: 0, max: 1},
+    //     metalness: {value: 1, min: 0, max: 1},
+    // })
 
     return(
         <Sphere args={[1, 256, 256]}>
-            <meshStandardMaterial {...tweakableProperties}/>
+            <meshStandardMaterial roughness={0.1} metalness={1}/>
         </Sphere>
     )
 }
 
 function ThreeScreen () {
+
+    const envMap = useEnvironment({files: '/golf_course/golf.hdr'});
+
     return(
         <>
             <ambientLight />
@@ -25,6 +28,7 @@ function ThreeScreen () {
             <pointLight position={[-3, -3, 2]}/>
             <OrbitControls/>
 
+            <Environment map={envMap} background/>
             <ReflectiveSphere/>
         </>
     );
