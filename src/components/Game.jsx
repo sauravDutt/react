a7e08f24a4b0c5cdf99e0169cc61e854798b79b8
sauravDutt <sauravd069@gmail.com>
2023-10-13 22:33:27 +0530

    Just added some more stuff

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,37 +1,6 @@
-import { Environment, OrbitControls, useEnvironment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-// import { useControls } from "leva";
+import GolfScreen from "./GolfScreen";
 
-// const ReflectiveSphere = () => {
-
-//     // const tweakableProperties = useControls({
-//     //     roughness: {value: 0.1, min: 0, max: 1},
-//     //     metalness: {value: 1, min: 0, max: 1},
-//     // })
-
-//     return(
-//         <Sphere args={[1, 256, 256]}>
-//             <meshStandardMaterial roughness={0.1} metalness={1}/>
-//         </Sphere>
-//     )
-// }
-
-function ThreeScreen () {
-
-    const envMap = useEnvironment({files: '/golf_course/golf.hdr'});
-
-    return(
-        <>
-            <ambientLight />
-            <pointLight position={[5, 5, 5]}/>
-            <pointLight position={[-3, -3, 2]}/>
-            <OrbitControls/>
-
-            <Environment map={envMap} background/>
-            {/* <ReflectiveSphere/> */}
-        </>
-    );
-}
 
 const Game = ()=> {
     return(
@@ -40,7 +9,7 @@ const Game = ()=> {
                 <div className="window">
                     <div className="golf-outter">
                         <Canvas className="mainCanvas">
-                            <ThreeScreen/>
+                            <GolfScreen/>
                         </Canvas>
                     </div>
                 </div>
---
+++ src/components/GolfScreen.js
@@ -0,0 +1,35 @@
+import { Environment, OrbitControls, useEnvironment } from "@react-three/drei";
+// import { useControls } from "leva";
+
+// const ReflectiveSphere = () => {
+
+//     // const tweakableProperties = useControls({
+//     //     roughness: {value: 0.1, min: 0, max: 1},
+//     //     metalness: {value: 1, min: 0, max: 1},
+//     // })
+
+//     return(
+//         <Sphere args={[1, 256, 256]}>
+//             <meshStandardMaterial roughness={0.1} metalness={1}/>
+//         </Sphere>
+//     )
+// }
+
+function GolfScreen () {
+
+    const envMap = useEnvironment({files: '/golf_course/golf.hdr'});
+
+    return(
+        <>
+            <ambientLight />
+            <pointLight position={[5, 5, 5]}/>
+            <pointLight position={[-3, -3, 2]}/>
+            <OrbitControls/>
+
+            <Environment map={envMap} background/>
+            {/* <ReflectiveSphere/> */}
+        </>
+    );
+}
+
+export default GolfScreen
