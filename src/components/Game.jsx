e64ba76d761c44a73e0e1858c132f98524c4727b
sauravDutt <sauravd069@gmail.com>
2023-10-14 22:07:45 +0530

    Just added some more stuff

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -36,7 +36,7 @@ const Game = ()=> {
                     <tr>
                         <td>1</td>
                         <td><input type="number" maxLength="2" pattern="[0-9]*" /></td>
-                        <td><input type="number" pattern="[0-9]*" className="yardsInput"/></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                         <td><input type="number" pattern="[0-9]*" /></td>
                         <td><input type="number" pattern="[0-9]*" /></td>
                         <td><input type="number" pattern="[0-9]*" /></td>
@@ -53,147 +53,147 @@ const Game = ()=> {
                     </tr>
                     <tr>
                         <td>3</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>4</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>5</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>6</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>7</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>8</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>9</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>10</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>11</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>12</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>13</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>14</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>15</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>16</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>17</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
                     <tr>
                         <td>18</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
+                        <td><input type="number" pattern="[0-9]*" /></td>
                     </tr>
 
                 </table>
