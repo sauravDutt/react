e0863cac52f083d43383fe2242312d84d8b9de23
sauravDutt <sauravd069@gmail.com>
2023-11-14 22:59:53 +0530

    Just added some more stuff

--- src/components/InfoGtown.jsx
+++ src/components/InfoGtown.jsx
@@ -1,5 +1,12 @@
 const InfoGtown = () => {
-  return <></>;
+  return (
+    <div className="info-outter">
+      <div>
+        <h1>info</h1>
+        <img src={process.env.PUBLIC_URL + "/img/infoImg.png"} alt="info" />
+      </div>
+    </div>
+  );
 };
 
 export default InfoGtown;
