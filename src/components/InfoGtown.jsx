69f558bcf092c1b06cb4ee758dff481366582e8e
sauravDutt <sauravd069@gmail.com>
2023-11-25 19:03:49 +0530

    Just added some more suff

--- src/components/InfoGtown.jsx
+++ src/components/InfoGtown.jsx
@@ -2,7 +2,8 @@ const InfoGtown = () => {
   return (
     <div className="info-outter">
       <div>
-        <h1>info</h1>
+        <h1>mint</h1>
+        <p></p>
         <img src={process.env.PUBLIC_URL + "/img/infoImg.png"} alt="info" />
       </div>
     </div>
