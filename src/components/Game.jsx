738b207f788425b3ceab63d3468de2234ae52e3e
sauravDutt <sauravd069@gmail.com>
2023-11-30 16:23:35 +0530

    Just added some more stuff

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -21,13 +21,15 @@ const PlayerOutter = () => {
 const Game = () => {
   return (
     <div className="margin-top-imp">
-      <div className="sceneOne"></div>
-      <div className="song-outter-outter">
-        <div className="play-outter">
-          <PlayerOutter />
+      <div className="podcast-outter">
+        <div className="sceneOne"></div>
+        <div className="song-outter-outter">
+          <div className="play-outter">
+            <PlayerOutter />
+          </div>
         </div>
+        <div className="comments-outter"></div>
       </div>
-      <div className="comments-outter"></div>
     </div>
   );
 };
