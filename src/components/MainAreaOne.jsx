e19adb22b60c50fd789d541ef6fa30d7cf17f2bb
sauravDutt <sauravd069@gmail.com>
2023-12-09 22:55:43 +0530

    Just added some more stuff

--- src/components/MainAreaOne.jsx
+++ src/components/MainAreaOne.jsx
@@ -93,6 +93,9 @@ const MainAreaOne = () => {
       />
       <div className="song-outter">
         <div className="songImg"></div>
+        <div className="song-title">
+          <h2>The Beginning - Podcast Episode 1</h2>
+        </div>
         <div className="play-outter">
           <PlayerOutter />
         </div>
