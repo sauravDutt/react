c0736391d25d9df2ff8c1f7d3d5b8aeb4402fb04
sauravDutt <sauravd069@gmail.com>
2024-01-29 20:02:11 +0530

    Just added some more stuff

--- src/components/GtownMap.jsx
+++ src/components/GtownMap.jsx
@@ -11,29 +11,29 @@ const Cube = () => {
   // });
   return (
     <>
-      <mesh position={[-1, 0.75, -1.54]}>
-        <boxGeometry args={[1, 1.5, 2]} />
+      <mesh position={[-2, 0.45, -2.5]}>
+        <boxGeometry args={[2, 1, 5]} />
         <meshStandardMaterial color={"#ffffff4f"} />
       </mesh>
     </>
   );
 };
-const CubeTwo = () => {
-  // const ref = useRef();
-  // useFrame((state, delta) => {
-  //   ref.current.rotation.y += delta;
-  //   ref.current.rotation.x += delta / 2;
-  //   ref.current.position.x = Math.sin(state.clock.elapsedTime) * 3.5;
-  // });
-  return (
-    <>
-      <mesh position={[0, 4, -4]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={"#ffffff4f"} />
-      </mesh>
-    </>
-  );
-};
+// const CubeTwo = () => {
+//   // const ref = useRef();
+//   // useFrame((state, delta) => {
+//   //   ref.current.rotation.y += delta;
+//   //   ref.current.rotation.x += delta / 2;
+//   //   ref.current.position.x = Math.sin(state.clock.elapsedTime) * 3.5;
+//   // });
+//   return (
+//     <>
+//       <mesh position={[0, 4.5, -5.2]}>
+//         <boxGeometry args={[1, 1, 1]} />
+//         <meshStandardMaterial color={"#ffffff4f"} />
+//       </mesh>
+//     </>
+//   );
+// };
 function ModelContent() {
   const gltf = useLoader(GLTFLoader, "/model/practice.gltf");
   return <primitive object={gltf.scene} />;
@@ -45,11 +45,10 @@ const GtownMap = () => {
       <Canvas camera={{ fov: 10, position: [0, 1, 32] }}>
         <gridHelper args={[100, 100, "0x000000"]} />
         <directionalLight position={[0, 0, 4]} />
-        <directionalLight position={[0, 0, 4]} />
+        <directionalLight position={[0, 4.5, -5.2]} />
         <ambientLight />
         <ModelContent />
         <Cube />
-        <CubeTwo />
         <OrbitControls />
       </Canvas>
     </>
