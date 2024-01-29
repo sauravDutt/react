298e74ec6780f71e05e98533e139fa41bbb5aa51
sauravDutt <sauravd069@gmail.com>
2024-01-29 19:54:40 +0530

    Just added some more stuff

--- src/components/GtownMap.jsx
+++ src/components/GtownMap.jsx
@@ -18,22 +18,22 @@ const Cube = () => {
     </>
   );
 };
-// const CubeTwo = () => {
-//   // const ref = useRef();
-//   // useFrame((state, delta) => {
-//   //   ref.current.rotation.y += delta;
-//   //   ref.current.rotation.x += delta / 2;
-//   //   ref.current.position.x = Math.sin(state.clock.elapsedTime) * 3.5;
-//   // });
-//   return (
-//     <>
-//       <mesh position={[1.5, 0.98, 0]}>
-//         <boxGeometry args={[1, 2, 1]} />
-//         <meshStandardMaterial color={"#ffffff4f"} />
-//       </mesh>
-//     </>
-//   );
-// };
+const CubeTwo = () => {
+  // const ref = useRef();
+  // useFrame((state, delta) => {
+  //   ref.current.rotation.y += delta;
+  //   ref.current.rotation.x += delta / 2;
+  //   ref.current.position.x = Math.sin(state.clock.elapsedTime) * 3.5;
+  // });
+  return (
+    <>
+      <mesh position={[0, 4, -4]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={"#ffffff4f"} />
+      </mesh>
+    </>
+  );
+};
 function ModelContent() {
   const gltf = useLoader(GLTFLoader, "/model/practice.gltf");
   return <primitive object={gltf.scene} />;
@@ -45,9 +45,11 @@ const GtownMap = () => {
       <Canvas camera={{ fov: 10, position: [0, 1, 32] }}>
         <gridHelper args={[100, 100, "0x000000"]} />
         <directionalLight position={[0, 0, 4]} />
+        <directionalLight position={[0, 0, 4]} />
         <ambientLight />
         <ModelContent />
         <Cube />
+        <CubeTwo />
         <OrbitControls />
       </Canvas>
     </>
