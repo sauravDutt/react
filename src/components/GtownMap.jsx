43456ccd21c8274ede8e006efdfd70f25974f00f
sauravDutt <sauravd069@gmail.com>
2024-01-29 19:37:12 +0530

    Just added some more stuff

--- src/components/GtownMap.jsx
+++ src/components/GtownMap.jsx
@@ -11,29 +11,29 @@ const Cube = () => {
   // });
   return (
     <>
-      <mesh position={[-1.5, 0.48, 0]}>
-        <boxGeometry args={[1, 1, 1]} />
+      <mesh position={[-1, 0.75, -1.54]}>
+        <boxGeometry args={[1, 1.5, 2]} />
         <meshStandardMaterial color={"#ffffff4f"} />
       </mesh>
     </>
   );
 };
-const CubeTwo = () => {
-  // const ref = useRef();
-  // useFrame((state, delta) => {
-  //   ref.current.rotation.y += delta;
-  //   ref.current.rotation.x += delta / 2;
-  //   ref.current.position.x = Math.sin(state.clock.elapsedTime) * 3.5;
-  // });
-  return (
-    <>
-      <mesh position={[1.5, 0.98, 0]}>
-        <boxGeometry args={[1, 2, 1]} />
-        <meshStandardMaterial color={"#ffffff4f"} />
-      </mesh>
-    </>
-  );
-};
+// const CubeTwo = () => {
+//   // const ref = useRef();
+//   // useFrame((state, delta) => {
+//   //   ref.current.rotation.y += delta;
+//   //   ref.current.rotation.x += delta / 2;
+//   //   ref.current.position.x = Math.sin(state.clock.elapsedTime) * 3.5;
+//   // });
+//   return (
+//     <>
+//       <mesh position={[1.5, 0.98, 0]}>
+//         <boxGeometry args={[1, 2, 1]} />
+//         <meshStandardMaterial color={"#ffffff4f"} />
+//       </mesh>
+//     </>
+//   );
+// };
 function ModelContent() {
   const gltf = useLoader(GLTFLoader, "/model/practice.gltf");
   return <primitive object={gltf.scene} />;
