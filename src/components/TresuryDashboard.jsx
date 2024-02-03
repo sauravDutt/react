0693d42a86222c045476049f26ba28e07cd655e7
sauravDutt <sauravd069@gmail.com>
2024-02-03 22:28:42 +0530

    Just added some more stuff

--- src/components/TresuryDashboard.jsx
+++ src/components/TresuryDashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import SendIcon from "@mui/icons-material/Send";
+// import SendIcon from "@mui/icons-material/Send";
 
 const TreasuryDashboard = ({ isAuth, userName, userImageUrl }) => {
   let navigate = useNavigate();
@@ -12,7 +12,7 @@ const TreasuryDashboard = ({ isAuth, userName, userImageUrl }) => {
   });
   return (
     <div className="treasury-outter">
-      <div className="treasury-head">
+      {/* <div className="treasury-head">
         <h1>Treasury</h1>
         <div>
           <div className="treasury-amount">
@@ -41,6 +41,15 @@ const TreasuryDashboard = ({ isAuth, userName, userImageUrl }) => {
             </button>
           </div>
         </div>
+      </div> */}
+      <div className="showcase-box">
+        <img src={process.env.PUBLIC_URL + "/oneOne.png"} alt="logo" />
+        <p>
+          Trusted real estate advisors since 1994, based in Delhi NCR, Gurugram
+          We have catered to more than 7,400 satisfied clients in our 30 years
+          of business.
+        </p>
+        <h3>www.realpoint.in</h3>
       </div>
     </div>
   );
