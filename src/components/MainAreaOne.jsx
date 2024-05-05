2f088f06faf60941df0e1f6f6a4cc5b32f7c98de
sauravDutt <sauravd069@gmail.com>
2024-05-05 11:31:17 +0530

    Just added some more stuff

--- src/components/MainAreaOne.jsx
+++ src/components/MainAreaOne.jsx
@@ -45,9 +45,10 @@ const MainAreaOne = () => {
           <div>
             <h1>Gtownjournal.com</h1>
             <p>
-              Gtown Journal, is a <strong>community</strong>, a consortium of
-              fine art enthusiasts, a decentralized network. A community built
-              and run by Gtown members. Scroll down to become a member of the{" "}
+              Gtown Journal, is a <strong>Digital Art Marketplace</strong> and a{" "}
+              <strong>community</strong>, a consortium of fine art enthusiasts,
+              a decentralized network. A community built and run by Gtown
+              members. Scroll down to become a member of the{" "}
               <strong>"Gtown Journal"</strong>.
             </p>
           </div>
