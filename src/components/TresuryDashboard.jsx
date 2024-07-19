207cb528ca76413311cc02e1453d3767cfccdb6e
sauravDutt <sauravd069@gmail.com>
2024-07-19 20:22:16 +0530

    Just added some more stuff

--- src/components/TresuryDashboard.jsx
+++ src/components/TresuryDashboard.jsx
@@ -7,6 +7,7 @@ import TouchAppIcon from "@mui/icons-material/TouchApp";
 import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
 import StorageIcon from "@mui/icons-material/Storage";
 import SellIcon from "@mui/icons-material/Sell";
+import PasswordIcon from "@mui/icons-material/Password";
 // import SendIcon from "@mui/icons-material/Send";
 
 function CarModalOne() {
@@ -82,6 +83,16 @@ const TreasuryDashboard = ({ isAuth, userName, userImageUrl }) => {
               <div className="treasury-amount">
                 <h2>£ 7,851.91</h2>
               </div>
+            </div>
+          </div>
+          <div className="comments-outter">
+            <p>
+              * This is a members only Utility, enter your membership id to
+              continue.
+            </p>
+            <div className="memberID-btn">
+              <PasswordIcon fontSize="large" sx={{ width: "100%" }} />
+              <input type="password" placeholder="Member ID"></input>
             </div>
           </div>
         </div>
