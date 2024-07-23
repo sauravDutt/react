de2562c817f36a570f4fa58f2c940876cedba54e
sauravDutt <sauravd069@gmail.com>
2024-07-23 12:07:14 +0530

    Just added some more stuff

---
+++ src/components/DigitalArt.jsx
@@ -0,0 +1,32 @@
+import { OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Canvas, useLoader } from "@react-three/fiber";
+
+function CarModalOne() {
+  const gltf = useLoader(GLTFLoader, "/model/Audi-R8.gltf");
+  return <primitive object={gltf.scene} />;
+}
+const DigitalArt = () => {
+  return (
+    <div className="digitalArt-outter">
+      <div>
+        <Canvas camera={{ fov: 20, position: [7, 10, -20] }}>
+          <gridHelper args={[50, 50, "0x000000"]} />
+          <directionalLight position={[0, 0, 4]} />
+          <directionalLight position={[0, 4.5, -5.2]} />
+          <CarModalOne />
+          <OrbitControls />
+        </Canvas>
+      </div>
+      <div className="digitalArt-info">
+        <h1>incomplete R8</h1>
+        <p>
+          <strong>Digital Artwork</strong> - "The incomplete" collection, <br />
+          by <i>Skank Hunt</i>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DigitalArt;
--- src/components/TresuryDashboard.jsx
+++ src/components/TresuryDashboard.jsx
@@ -1,19 +1,12 @@
 import { motion } from "framer-motion";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { OrbitControls } from "@react-three/drei";
 import SkipNextIcon from "@mui/icons-material/SkipNext";
 import TouchAppIcon from "@mui/icons-material/TouchApp";
 import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
 import StorageIcon from "@mui/icons-material/Storage";
 import SellIcon from "@mui/icons-material/Sell";
 import PasswordIcon from "@mui/icons-material/Password";
+import DigitalArt from "./DigitalArt";
 // import SendIcon from "@mui/icons-material/Send";
-
-function CarModalOne() {
-  const gltf = useLoader(GLTFLoader, "/model/Audi-R8.gltf");
-  return <primitive object={gltf.scene} />;
-}
 
 const TreasuryDashboard = ({ isAuth, userName, userImageUrl }) => {
   return (
@@ -55,25 +48,7 @@ const TreasuryDashboard = ({ isAuth, userName, userImageUrl }) => {
           <SkipNextIcon fontSize="large" sx={{ width: "90%" }} />
         </motion.div>
       </div>
-      <div className="digitalArt-outter">
-        <div>
-          <Canvas camera={{ fov: 20, position: [7, 10, -20] }}>
-            <gridHelper args={[50, 50, "0x000000"]} />
-            <directionalLight position={[0, 0, 4]} />
-            <directionalLight position={[0, 4.5, -5.2]} />
-            <CarModalOne />
-            <OrbitControls />
-          </Canvas>
-        </div>
-        <div className="digitalArt-info">
-          <h1>incomplete R8</h1>
-          <p>
-            <strong>Digital Artwork</strong> - "The incomplete" collection,{" "}
-            <br />
-            by <i>Skank Hunt</i>
-          </p>
-        </div>
-      </div>
+      <DigitalArt />
 
       <div className="memberCard-outter">
         <div className="card-outter">
