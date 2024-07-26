17eb494411cf6b4f31761f570da7f12cef6d1c00
sauravDutt <sauravd069@gmail.com>
2024-07-26 19:44:06 +0530

    Just added some more stuff

--- src/components/InfoGtown.jsx
+++ src/components/InfoGtown.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import GtownMap from "./GtownMap";
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
+import { Center } from "@react-three/drei";
+import { bottomNavigationActionClasses } from "@mui/material";
 
 const InfoGtown = ({ setIsAuth, isAuth }) => {
   let navigate = useNavigate();
@@ -15,24 +17,50 @@ const InfoGtown = ({ setIsAuth, isAuth }) => {
     });
   };
 
-  const ShowGoogleBtn = () => {
-    return (
-      <button className="login-with-google-btn" onClick={signInWithGoogle}>
-        Signin
-      </button>
-    );
-  };
-
   const UploadArticle = () => {
     return (
       <div
         style={{
-          border: "1px solid #000",
-          height: "60px",
           width: "90%",
           margin: "0 auto",
+          display: "grid",
+          gridTemplateColumns: "auto",
+          justifyContent: "Center",
+          alignItems: "center",
+          gap: "20px",
         }}
-      ></div>
+      >
+        <h2 style={{ marginTop: "10px", marginBottom: "0px" }}>Step Two</h2>
+        <p
+          style={{
+            marginTop: "-5px",
+            marginBottom: "5px",
+          }}
+        >
+          Give your article a topic.
+        </p>
+        <input type="text" className="articleTitle"></input>
+
+        <textarea className="textAreaArticle" cols={35}></textarea>
+        <button style={{ marginTop: "-10px", width: "100px" }}>Submit</button>
+      </div>
+    );
+  };
+
+  const ShowGoogleBtn = () => {
+    return (
+      <div>
+        <h2>Become an Editor</h2>
+        <p>
+          Join the Editorial board by simply, submit an original article with an
+          illustration. an illustration.
+        </p>
+        <p>Sign in with your google account and navigate back to this page.</p>
+
+        <button className="login-with-google-btn" onClick={signInWithGoogle}>
+          Signin
+        </button>
+      </div>
     );
   };
 
@@ -44,17 +72,7 @@ const InfoGtown = ({ setIsAuth, isAuth }) => {
           <h1>editorial board</h1>
         </div>
         <div className="info-gtown-map">
-          <div>
-            <h2>Become an Editor</h2>
-            <p>
-              Join the Editorial board by simply, submit an original article
-              with an illustration. an illustration.
-            </p>
-            <p>
-              Sign in with your google account and navigate back to this page.
-            </p>
-            {isAuth ? <UploadArticle /> : <ShowGoogleBtn />}
-          </div>
+          <div>{isAuth ? <UploadArticle /> : <ShowGoogleBtn />}</div>
           <div>
             <GtownMap />
           </div>
