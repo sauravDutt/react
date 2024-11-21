61824f5bc3447f45aa8eb55e7b799194c8c63ad0
sauravDutt <sauravd069@gmail.com>
2024-11-21 02:41:10 +0000

    Just added some more stuff

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
         <div className="app-title">
           <h1 className="brand-name">Good Time Journal</h1>
           <h3 className="lower">
-            a portfolio build in <b>2023</b>.
+            a portfolio build in <b>2024</b>.
           </h3>
         </div>
       </div>
