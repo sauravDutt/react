c5f9e5908261feb1dd715f810aa570db6c7d2461
sauravDutt <sauravd069@gmail.com>
2025-01-22 17:55:07 +0000

    Just added some more stuff

--- src/components/Bts.jsx
+++ src/components/Bts.jsx
@@ -13,14 +13,7 @@ const Bts = () => {
             </div>
             
             <div className="bts-products-div">
-                {/* <div className="bts-card">
-                    <div className="bts-btn-outter">
-                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}></motion.div>
-                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}></motion.div>
-                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}></motion.div>
-                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}></motion.div>
-                    </div>
-                </div> */}
+                
                 <div className="bts-card-one"> 
                     <img src={process.env.PUBLIC_URL + "/img/Part2.png"} alt="Img" srcset="" />
                     <img src={process.env.PUBLIC_URL + "/img/ChairDesignOne.png"} alt="img" srcset="" className="floatRight"/>
