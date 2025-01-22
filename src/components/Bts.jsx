b08bb1db114ecf28858f9115ef8ee3af573d420e
sauravDutt <sauravd069@gmail.com>
2025-01-22 18:00:34 +0000

    Just added some more stuff

--- src/components/Bts.jsx
+++ src/components/Bts.jsx
@@ -13,7 +13,14 @@ const Bts = () => {
             </div>
             
             <div className="bts-products-div">
-                
+                {/* <div className="bts-card">
+                    <div className="bts-btn-outter">
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}></motion.div>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}></motion.div>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}></motion.div>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}></motion.div>
+                    </div>
+                </div> */}
                 <div className="bts-card-one"> 
                     <img src={process.env.PUBLIC_URL + "/img/Part2.png"} alt="Img" srcset="" />
                     <img src={process.env.PUBLIC_URL + "/img/ChairDesignOne.png"} alt="img" srcset="" className="floatRight"/>
@@ -21,7 +28,7 @@ const Bts = () => {
                     <img src={process.env.PUBLIC_URL + "/img/download.png"} alt="Img" srcset="" className="floatRight"/>
                     <img src={process.env.PUBLIC_URL + "/img/crosstrainerOne.png"} alt="Img" srcset="" />
                     <img src={process.env.PUBLIC_URL + "/img/crosstrainerTwo.png"} alt="Img" srcset="" className="floatRight"/>
-                    <p>Product Design, <span className="smallText">2024-2025</span></p>
+                    <p>Product Design, <br/><span className="smallText">2024-2025</span></p>
                     <img src={process.env.PUBLIC_URL + "/img/csc.png"} alt="Img" srcset="" />
                 </div>
                 
