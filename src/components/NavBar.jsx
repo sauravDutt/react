a4d91649e5e0c57cf05d512473b0ed143dbccfb0
sauravDutt <sauravd069@gmail.com>
2025-03-07 23:17:32 +0000

    Jst

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,16 +3,12 @@ const NavBar = () => {
     <div className="navbar-outter">
       <div className="outBox">
         <div className="app-title">
-          <br/>
-          <br/>
-          <br/>
-          <h1 className="brand-name">Good Time<span className="secondfont"></span> </h1>
+          
+          <h1 className="brand-name">Good Time</h1>
           <h3 className="lower">
             a portfolio build in <b>2024</b>.
           </h3>
-          <br/>
-          <br/>
-          <br/>
+          
         </div>
       </div>
     </div>
