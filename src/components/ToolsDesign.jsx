fc0f01245f53c28e6b1ed8f3eca45188bc12a85e
sauravDutt <sauravd069@gmail.com>
2025-04-18 02:04:28 +0100

    Just added some more stuff

--- src/components/ToolsDesign.jsx
+++ src/components/ToolsDesign.jsx
@@ -3,6 +3,8 @@ const ToolsDesign = () => {
     return(
         <div className="Folio-outter">
             <div><h1 className="folioTitle">GoodTime<span className="seconfont">/Tools</span></h1></div>
+            <div className="tools-first-div"></div>
+            <div></div>
             <div></div>
             <div></div>
         </div>
