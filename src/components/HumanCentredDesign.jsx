6f36a7223b4759267f04cb399fe0d2baf4bb5065
sauravDutt <sauravd069@gmail.com>
2025-04-22 11:09:16 +0100

    Just added some more stuff

--- src/components/HumanCentredDesign.jsx
+++ src/components/HumanCentredDesign.jsx
@@ -2,8 +2,8 @@
 const HumanCentredDesign = () => {
     return(
         <div className="Folio-outter">
-            <div><h1 className="folioTitle">GoodTime<span className="seconfont">/HCD #1</span></h1></div>
-            <div></div>
+            <div><h1 className="folioTitle">Health Psychology<span className="seconfont">/Self Assessment</span></h1></div>
+            <div className="firstDiv-psychology"></div>
             <div></div>
         </div>
     )
