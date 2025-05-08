56ed2b5b6a4874586987f344c7fcca00829c88bc
sauravDutt <sauravd069@gmail.com>
2025-05-08 02:50:10 +0100

    Just added some more stuff

--- src/components/LCAestimationInCD.jsx
+++ src/components/LCAestimationInCD.jsx
@@ -3,11 +3,12 @@
 const LCAestimationInCD = () => {
     return(
         <div className="Folio-outter">
-            <div><h1 className="folioTitle">GoodTime<span className="seconfont">/LCA</span></h1></div>
+            <div><h1 className="folioTitle">GoodTime<span className="seconfont">/LCAC</span></h1></div>
             <div className="lca-banner">
                 <div></div>
                 <div className="right-lca">
                     <h1>Life <br/>Cycle <br/>Assessment <br/>Calculator</h1>
+                    <p>for estimation in conceptual design.</p>
                     <button>Try Application</button>
                 </div>
             </div>
