48eb35f159917d28a6c24f1c61f2aa53c490ca7a
sauravDutt <sauravd069@gmail.com>
2025-05-28 17:47:18 +0100

    Just added some more stuff

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,8 +3,7 @@ const Game = () => {
   return (
     <div className="margin-top-imp">
       <div className="lcac-outter">
-        <h1>life cycle assessment</h1>
-      <div className="tag-calculator"><strong>Calculator</strong></div>
+        <h1><span className="second-font">life cycle assessment calculator </span>(LCAC)</h1>
       </div>
       <div className="lcac-main-outter">
 
