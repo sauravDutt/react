b6fdba8ee6432822abc073d8b751072845c51e9a
sauravDutt <sauravd069@gmail.com>
2025-08-29 00:06:04 +0100

    Just added some more stuff

--- src/components/Chair.jsx
+++ src/components/Chair.jsx
@@ -1,7 +1,14 @@
 const Chair = () => {
     return(
         <div className="chair-Outter">
-            <div></div>
+            <div className="chair-brief">
+                <h1>
+                    Design Form and Aesthetics, “good time design chair”
+                </h1>
+                <p>
+                    The objective is to express personal aesthetics and design a prototype of a chair. The folio is structured in the following order. 
+                </p>
+            </div>
             <div></div>
             <div></div>
             <div></div>
