78c1531adab7232cc5b5440202175a503b3a4f97
sauravDutt <sauravd069@gmail.com>
2025-08-29 00:30:14 +0100

    Just added some more stuff

--- src/components/Chair.jsx
+++ src/components/Chair.jsx
@@ -8,9 +8,28 @@ const Chair = () => {
                 <p>
                     The objective is to express personal aesthetics and design a prototype of a chair. The folio is structured in the following order. 
                 </p>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <p style={{textAlign:"right", marginRight: "25px"}}>----------{'>'}</p>
             </div>
-            <div className="chair-structure"></div>
-            <div></div>
+            <div className="chair-structure">
+                <ol>
+                    <li>Design Inspiration</li>
+                    <li>Mind Map of Personal Aesthetics </li>
+                    <li>Personal Aesthetics Mood Board </li>
+                    <li>Manifesto</li>
+                    <li>Ideation Sketches</li>
+                    <li>Prototype Concept </li>
+                    <li>Final Model Process</li>
+                </ol>
+            </div>
+            <div>
+                
+            </div>
             <div></div>
         </div>
     )
