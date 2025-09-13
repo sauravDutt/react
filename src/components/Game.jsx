543995c10929e8ef3900d19baba8f3c4e13c5673
sauravDutt <sauravd069@gmail.com>
2025-09-13 17:28:53 +0100

    Just added some more stuff

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,6 +8,9 @@ const Game = () => {
     <div className="margin-top-imp">
       <div className="lcac-outter">
         <h1><span className="second-font">Good time design <br/></span>( GTD / about )</h1>
+        <p>
+          Good Time Design (GTD), is an online design market place 
+        </p>
       </div>
 
     </div>
