538f1df58adeeb38c268ef872f3995cd8c9de7a2
sauravDutt <sauravd069@gmail.com>
2025-09-28 04:33:06 +0100

    Just added some more stuff

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,10 +8,7 @@ const Game = () => {
     <div className="margin-top-imp">
       <div className="lcac-outter">
         <h1><span className="second-font">Good time design <br/></span>( GTD / about )</h1>
-        <p>
-          Good Time Design (GTD), is an online design market place for independent designers 
-          XXXXXX XXXXXXXXX XXXXXX XXXXX XXXXXXXX XXXX 
-          XXXXXXX XXXXX XXXXXX XXXXX XXXXX XXXXXX XXXXX XXXXXX XXXXX XXXXX XXXXX.
+        <p>         Good Time Design (GTD), is an online consortium of digital creators (illustrators, designers, artists) on a decentralised platform for transparent trade of original digital ideas (intellectual properties), a community driven by creativity, innovation and storytelling.
         </p>
       </div>
 
