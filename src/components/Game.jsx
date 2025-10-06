4d51eeba9f0737b9e10c6c446c11f868421c139e
sauravDutt <sauravd069@gmail.com>
2025-10-06 22:17:34 +0100

    Just added some more stuff

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,6 +8,7 @@ const Game = () => {
     <div className="margin-top-imp">
       <div className="lcac-outter">
         <h1><span className="second-font">Good time design <br/></span>( GTD / about )</h1>
+        <br />
         <p>
           Good Time Design (GTD) is an online community of fine art enthusiasts, a consortium between independent designers, manufacturers, digital creators and artists, providing a transparent trade of digital (tangible) assets.
         </p>
